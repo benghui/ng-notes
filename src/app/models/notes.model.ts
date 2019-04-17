afe260fe6f121fb069765a336b55e6abcbb8f5be
benghui <benghui@gmail.com>
2019-04-17 16:02:30 +0700

    updated app module & added notes model

---
+++ src/app/models/notes.model.ts
@@ -0,0 +1,9 @@
+export class notes {
+    title: string;
+    content: string;
+
+    // constructor(title: string, content: string) {
+    //     this.title = title;
+    //     this.content = content;
+    // }
+}
